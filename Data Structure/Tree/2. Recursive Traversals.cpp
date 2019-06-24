c45d267840be07665e13d5eacf6c789a7f300b61
VeerRavi97 <veerravi258@gmail.com>
2019-06-24 12:09:56 +0530

    traversals added

--- Data Structure/Tree/2. Recursive Traversals.cpp
+++ Data Structure/Tree/2. Recursive Traversals.cpp
@@ -49,11 +49,14 @@ root->left->left=new Node(4);
 root->left->right=new Node(5);
 root->left->right->left=new Node(7);
 root->right->right=new Node(6);
- Solution MyTree;
+ Solution MyTree; 
+ cout << "Inorder :" ;
  MyTree.Inorder(root);
  cout << "\n";
+ cout << "Preorder :" ;
  MyTree.Preorder(root);
   cout << "\n";
+  cout << "Postorder :" ;
  MyTree.Postorder(root);
 
 }

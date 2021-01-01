ffee88b222cd06f201f5e538f70a73cb2c63a113
VeerRavi97 <veerravi258@gmail.com>
2021-01-01 07:36:43 +0530

    From Past

--- Data Structure & Algorithm/Linked List/0.BolierPlate.cpp
+++ Data Structure & Algorithm/Linked List/0.BolierPlate.cpp
@@ -1,60 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
-    int val;
+class Node
+{
+public:
+    int data;
     Node *next;
-     Node(int val){
-        this->val=val;
-        this->next=NULL;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
     }
 };
-
-class Solution{
-    public:
-
-   Node *insert(Node *head,int val){
-       Node *nn=new Node(val);
-       if(!head){
-           head=nn;
-           return head;
-       }
-       Node *ptr=head;
-       while(ptr->next){
-           ptr=ptr->next;
-       }
-       ptr->next=nn;
-       return head;
-   }
- void display(Node *head){
-     Node *ptr=head;
-     while(ptr){
-         cout << ptr->val << " ";
-         ptr=ptr->next;
-     }
-     cout << "\n";
- }
-
- Node *FunctionName(Node *head){
-
-
-
- }
-
-
-};
-int main(){
-Node *head=NULL;
-Solution obj;
-int n;
-cin >> n;
-for(int i=0;i<n;i++){
-int in;
-cin >> in;
-head=obj.insert(head,in);
-}
-
-head=obj.FunctionName(head);
-obj.display(head);
-
+int main()
+{
 }
--- Data Structure & Algorithm/Linked List/1.b)InsertatEnd.cpp
+++ Data Structure & Algorithm/Linked List/1.b)InsertatEnd.cpp
@@ -1,49 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Node{
-    public:
+class Node
+{
+public:
     int val;
     Node *next;
-     Node(int val){
-        this->val=val;
-        this->next=NULL;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
     }
 };
 
-class Solution{
-    public:
+class Solution
+{
+public:
+    Node *insert(Node *head, int val)
+    {
 
-   Node *insert(Node *head,int val){
-       Node *nn=new Node(val);
-       if(!head){
-           head=nn;
-           return head;
-       }
-       Node *ptr=head;
-       while(ptr->next){
-           ptr=ptr->next;
-       }
-       ptr->next=nn;
-       return head;
-   }
- void display(Node *head){
-     Node *ptr=head;
-     while(ptr){
-         cout << ptr->val << " ";
-         ptr=ptr->next;
-     }
- }
-
-
+        if (!head)
+        {
+            Node *nn = new Node(val);
+            head = nn;
+            return head;
+        }
+        Node *nn = new Node(val);
+        Node *tail = head;
+        while (tail->next)
+        {
+            tail = tail->next;
+        }
+        tail->next = nn;
+        return head;
+    }
+    void display(Node *head)
+    {
+        Node *ptr = head;
+        while (ptr)
+        {
+            cout << ptr->val << " ";
+            ptr = ptr->next;
+        }
+    }
 };
-int main(){
-Node *head=NULL;
-Solution obj;
-head=obj.insert(head,1);
-head=obj.insert(head,2);
-head=obj.insert(head,3);
-head=obj.insert(head,4);
-head=obj.insert(head,5);
-obj.display(head);
-
+int main()
+{
+    Node *head = NULL;
+    Solution obj;
+    head = obj.insert(head, 1);
+    head = obj.insert(head, 2);
+    head = obj.insert(head, 3);
+    head = obj.insert(head, 4);
+    head = obj.insert(head, 5);
+    obj.display(head);
 }
